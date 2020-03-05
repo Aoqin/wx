78ee6b286c49160b05ca2bdee4a40076feb2d11e
unknown <unknown@example.com>
2020-03-05 22:41:27 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,46 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    oparList: [
+      
+    ],
+    list: [
+
+    ],
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 1000,
+    duration: 500
+  },
+  onLoad: function () {
+
+  },
+  changeIndicatorDots() {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+
+  changeAutoplay() {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+})
